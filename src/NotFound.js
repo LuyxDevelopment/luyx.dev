d663f51c8e5a4d6b62547c0d365660283a8f9f86
Jaren Goldberg <fyrlex@gmail.com>
2022-01-07 11:43:38 -0500

    push

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,16 +1,16 @@
 import Cube from "./assets/images/cube.svg";
 
 function NotFound() {
-    return (<>
-        <div id="content" className="h-screen w-screen grid items-center">
-            <div>
-                <img alt="Bremea development small" src={Cube} className="mx-auto h-16 md:h-32" />
-                <h1 className="text-2xl md:text-4xl text-center mt-4">
-                    This page doesn't exist.
-                </h1>
-            </div>
-        </div>
-    </>);
+	return (<>
+		<div id="content" className="h-screen w-screen grid items-center">
+			<div>
+				<img alt="Luyx Development Small" src={Cube} className="mx-auto h-16 md:h-32" />
+				<h1 className="text-2xl md:text-4xl text-center mt-4">
+					This page doesn't exist.
+				</h1>
+			</div>
+		</div>
+	</>);
 }
 
 export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes
+	BrowserRouter as Router,
+	Route,
+	Routes
 } from "react-router-dom";
 
 import "./assets/fonts/font.ttf";
@@ -15,19 +15,19 @@ import NotFound from "./NotFound";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/" element={<App />} />
-        <Route path="/discord" element={() => {
-          window.location.href = "https://discord.gg/jRUWbXhCYN";
-          return null;
-        }} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path="*" element={<NotFound />} />
+				<Route path="/terms" element={<Terms />} />
+				<Route path="/privacy" element={<Privacy />} />
+				<Route path="/" element={<App />} />
+				<Route path="/discord" element={() => {
+					window.location.href = "https://discord.gg/jRUWbXhCYN";
+					return null;
+				}} />
+			</Routes>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
