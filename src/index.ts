16576460c70d9649735c0122368c16234140caf9
Fyrlex <fyrlex@gmail.com>
2022-12-02 06:52:31 -0500

    fix

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { LuyxClient } from 'luyx-management-wrapper';
-
-export const client = new LuyxClient('y9DKvjHrqQ/YNbQ2zw7nK9WgJ4dX7+KX1khDWSdoA3bPGmBJQGi0eTtpwf6+pSObKwKok3cJSzDWx4yJia+BAA==');
