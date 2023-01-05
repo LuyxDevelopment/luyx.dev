8159e00a1fe7d46906d3967d834434ec75810c90
Fyrlex <fyrlex@gmail.com>
2023-01-04 21:52:37 -0500

    fix: conditional render

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -8,26 +8,48 @@ interface Props {
 }
 
 const UserCard: FC<Props> = ({ user }) => {
+
+	if (user.contact.gitHub) {
+		return (
+			<div className='hover:-translate-y-2 duration-300'>
+				<Link
+					href={`https://github.com/${user.contact.gitHub}`}
+				>
+					<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
+						<Image
+							alt={user.alias}
+							src={user.avatarURL}
+							className='rounded-md w-24 mr-4'
+							height={256}
+							width={256}
+						/>
+						<div className='w-full'>
+							<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
+							<p className='text-s text-ellipsis font-bold'>{user.position}</p>
+							<p>{user.info.countryCode}</p>
+						</div>
+					</div>
+				</Link>
+			</div>
+		);
+	}
+
 	return (
 		<div className='hover:-translate-y-2 duration-300'>
-			<Link
-				href={`https://github.com/${user.contact.gitHub}`}
-			>
-				<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
-					<Image
-						alt={user.alias}
-						src={user.avatarURL}
-						className='rounded-md w-24 mr-4'
-						height={256}
-						width={256}
-					/>
-					<div className='w-full'>
-						<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
-						<p className='text-s text-ellipsis font-bold'>{user.position}</p>
-						<p>{user.info.countryCode}</p>
-					</div>
+			<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
+				<Image
+					alt={user.alias}
+					src={user.avatarURL}
+					className='rounded-md w-24 mr-4'
+					height={256}
+					width={256}
+				/>
+				<div className='w-full'>
+					<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
+					<p className='text-s text-ellipsis font-bold'>{user.position}</p>
+					<p>{user.info.countryCode}</p>
 				</div>
-			</Link>
+			</div>
 		</div>
 	);
 };
