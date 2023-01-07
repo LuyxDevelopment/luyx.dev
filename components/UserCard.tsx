8435fdd4ee7054bcd1f11bfec501aa4d66144289
Marco <me@marco.win>
2023-01-07 15:13:57 +0100

    Emojis instead of country code

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,55 +1,41 @@
 import { APIUser } from 'luyx-management-api-types/v1/index.js';
+import getFlagEmoji from '../data/CountryToEmoji';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, ReactElement } from 'react';
 
 interface Props {
 	user: APIUser;
 }
 
-const UserCard: FC<Props> = ({ user }) => {
-
-	if (user.contact.gitHub) {
-		return (
-			<div className='hover:-translate-y-2 duration-300'>
-				<Link
-					href={`https://github.com/${user.contact.gitHub}`}
-				>
-					<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
-						<Image
-							alt={user.alias}
-							src={user.avatarURL}
-							className='rounded-md w-24 mr-4'
-							height={256}
-							width={256}
-						/>
-						<div className='w-full'>
-							<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
-							<p className='text-s text-ellipsis font-bold'>{user.position}</p>
-							<p>{user.info.countryCode}</p>
-						</div>
-					</div>
-				</Link>
-			</div>
-		);
+const DynamicLink: FC<{ href: string | null, children: ReactElement }> = ({ href, children }) => {
+	if (href) {
+		return <Link href={href}>{children}</Link>;
 	}
+	return children;
+};
 
+const UserCard: FC<Props> = ({ user }) => {
 	return (
 		<div className='hover:-translate-y-2 duration-300'>
-			<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
-				<Image
-					alt={user.alias}
-					src={user.avatarURL}
-					className='rounded-md w-24 mr-4'
-					height={256}
-					width={256}
-				/>
-				<div className='w-full'>
-					<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
-					<p className='text-s text-ellipsis font-bold'>{user.position}</p>
-					<p>{user.info.countryCode}</p>
+			<DynamicLink
+				href={user.contact.gitHub ? `https://github.com/${user.contact.gitHub}` : null}
+			>
+				<div className='p-4 bg-egg-sour shadow-lg ring-1 ring-gray-200/30 rounded-md flex cursor-pointer'>
+					<Image
+						alt={user.alias}
+						src={user.avatarURL}
+						className='rounded-md w-24 mr-4'
+						height={256}
+						width={256}
+					/>
+					<div className='w-full'>
+						<p className='text-2xl text-ellipsis title font-bold'>{user.alias}</p>
+						<p className='text-s text-ellipsis font-bold'>{user.position}</p>
+						<p>{getFlagEmoji(user.info.countryCode)}</p>
+					</div>
 				</div>
-			</div>
+			</DynamicLink>
 		</div>
 	);
 };
