c5bed237eff1b903700297a89ae935779f346b90
Jaren Goldberg <fyrlex@gmail.com>
2023-08-01 13:30:43 -0400

    fix

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+	import '../app.css';
+
+	const pages: { name: string; url: string }[] = [
+		{ name: 'home', url: '' },
+		{ name: 'hobbies', url: 'hobbies' },
+		{ name: 'programming', url: 'programming' }
+	];
+</script>
+
+<nav
+	class="flex space-x-6 bg-stone-900 font-light text-center text-2xl justify-center items-center h-20"
+>
+	{#each pages as { name, url }, i}
+		<a class="text-[#F25922] hover:text-cyan-400 duration-300" href="{base}/{url}">{name}</a>{/each}
+</nav>
+
+<slot />
